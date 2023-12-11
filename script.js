d3b05afdfd7985a8560c030e154c8309f4baab20
Maksim <pryan1kgamingbro@gmail.com>
2023-12-11 13:31:59 +0300

    test 0.2

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
-function drawGraph() {
-
+function addSquare(color) {
+    let container = document.getElementById('colorSquaresContainer');
+    let square = document.createElement('div');
+    square.className = 'color-square';
+    square.style.backgroundColor = color;
+    container.appendChild(square);
 }
+
+function removeFirstSquare() {
+    let container = document.getElementById('colorSquaresContainer');
+    let squares = container.getElementsByClassName('color-square');
+    if (squares.length > 0) {
+        container.removeChild(squares[0]);
+    }
+}
+
+function removeLastSquare() {
+    let container = document.getElementById('colorSquaresContainer');
+    let squares = container.getElementsByClassName('color-square');
+    let lastIndex = squares.length - 1;
+    if (lastIndex >= 0) {
+        container.removeChild(squares[lastIndex]);
+    }
+}
+    function removeSquare() {
+        const container = document.getElementById('colorSquaresContainer');
+        var colorRemove = document.getElementById('colorRemove').value.toLowerCase();
+        let squares = container.getElementsByClassName('color-square');
+        let j = 0;
+        for (let i = squares.length - 1; i >= 0; i--) 
+        {
+            var squareColor = squares[i].style.backgroundColor.toLowerCase();
+            if (squareColor === colorRemove) {
+                container.removeChild(squares[i]);
+                j++;
+            }
+            
+        }
+        if (j == 0)
+        {
+            alert('Цвета нету!')
+        }
+    }
