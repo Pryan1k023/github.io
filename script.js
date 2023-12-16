24dd14afc72dddc5bf42d25ec4877c1982169652
Maksim <pryan1kgamingbro@gmail.com>
2023-12-16 19:43:12 +0300

    testing!

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-function myVk() {
-    if (document.getElementById == "myGit")
-  {
-    window.open("https://github.com/Pryan1k023", _blank);
-  }
-  else if (document.getElementById == "mySyte")
-  {
-    window.open("https://pryan1k023.github.io/pryan1k-syte/", _blank);
-  }
-  else if(document.getElementById == "mySol")
-  {
-    window.open("https://www.sololearn.com/ru/profile/25516665", _blank);
-  }
-  else if(document.getElementById == "myVk")
-  {
-    window.open("https://vk.com/maksimka023");
-  }
+function myLink(link) {
+ 
+  const linkInfo = link == "myGit" ? "https://github.com/Pryan1k023" :
+  link == "mySyte" ? "https://pryan1k023.github.io/pryan1k-syte/" :
+  link == "mySol" ? "https://www.sololearn.com/ru/profile/25516665" :
+  link == "myVk" ? "https://vk.com/maksimka023" :
+  "";
+  window.open(linkInfo, '_blank').focus();
 }
