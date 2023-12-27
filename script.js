44ae6e26f6f40b7b315ef28d5bae508b94d6e129
Maksim <pryan1kgamingbro@gmail.com>
2023-12-27 11:07:40 +0300

    test
    new testing

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 function myLink(link) {
  
   const linkInfo = link == "myGit" ? "https://github.com/Pryan1k023" :
-  link == "mySyte" ? "https://pryan1k023.github.io/pryan1k-syte/" :
-  link == "mySol" ? "https://www.sololearn.com/ru/profile/25516665" :
   link == "myVk" ? "https://vk.com/maksimka023" :
   "";
   window.open(linkInfo, '_blank').focus();
